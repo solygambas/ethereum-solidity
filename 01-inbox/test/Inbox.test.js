2879596147f0df01485d740ad361594d4ebbd483
solygambas <51904909+solygambas@users.noreply.github.com>
2021-11-17 17:05:51 +0100

    add testing setup with mocha, web3 and ganache

--- 01-inbox/test/Inbox.test.js
+++ 01-inbox/test/Inbox.test.js
@@ -3,3 +3,24 @@ const ganache = require("ganache-cli");
 const Web3 = require("web3");
 const provider = ganache.provider();
 const web3 = new Web3(provider);
+const { abi, evm } = require("../compile"); // interface and bytecode
+
+const message = "Hi there!"; // initial message
+let accounts;
+let inbox;
+
+beforeEach(async () => {
+  // Get a list of all accounts
+  accounts = await web3.eth.getAccounts();
+  // Use one of those accounts to deploy the contract.
+  inbox = await new web3.eth.Contract(abi)
+    .deploy({ data: "0x" + evm.bytecode.object, arguments: [message] })
+    .send({ from: accounts[0], gas: "1000000" });
+});
+
+describe("Inbox", () => {
+  it("deploys a contract", () => {
+    console.log(inbox);
+    // assert.ok(inbox.options.address);
+  });
+});
--- README.md
+++ README.md
@@ -20,6 +20,8 @@ x
 
 - setting up a project in VS Code with [Solidity](https://marketplace.visualstudio.com/items?itemName=JuanBlanco.solidity) syntax highlighting.
 - creating a compile script for Solidity with solc.
-- testing locally with Mocha, Ganache CLI and web3.
+- testing the contract with Mocha.
+- fetching accounts from Ganache CLI.
+- deploying locally with web3.
 
 Based on [Ethereum and Solidity: The Complete Developer's Guide](https://www.udemy.com/course/ethereum-and-solidity-the-complete-developers-guide/) by Stephen Grider (2021).
