34bb00ac6f451e12c8de5920a3bb07d312deffa3
solygambas <51904909+solygambas@users.noreply.github.com>
2021-11-19 12:34:06 +0100

    add deploying

--- 01-inbox/README.md
+++ 01-inbox/README.md
@@ -1,6 +1,6 @@
 # Inbox
 
-x
+An intro to understand how to test and deploy smart contracts with Solidity.
 
 ## Features
 
@@ -9,5 +9,7 @@ x
 - testing the contract with Mocha.
 - fetching accounts from Ganache CLI.
 - deploying locally with web3.
+- deploying to Rinkeby network with hdwallet-provider and Infura API.
+- observing deployment on Etherscan and testing the new contract in Remix.
 
 Based on [Ethereum and Solidity: The Complete Developer's Guide](https://www.udemy.com/course/ethereum-and-solidity-the-complete-developers-guide/) by Stephen Grider (2021).
--- 01-inbox/deploy.js
+++ 01-inbox/deploy.js
@@ -1,2 +1,32 @@
+// Load environment variables.
+require("dotenv").config();
 
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const Web3 = require("web3");
+const { abi, evm } = require("./compile");
+const mnemonicPhrase = process.env.ACCOUNT_MNEMONIC;
+const network = process.env.RINKEBY_ENDPOINT;
 
+const provider = new HDWalletProvider({
+  mnemonic: {
+    phrase: mnemonicPhrase,
+  },
+  providerOrUrl: network,
+});
+const web3 = new Web3(provider);
+const message = "Hi there!";
+
+console.log(mnemonicPhrase);
+console.log(network);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+  console.log("Attempting to deploy from account", accounts[0]);
+  const result = await new web3.eth.Contract(abi)
+    .deploy({ data: "0x" + evm.bytecode.object, arguments: [message] })
+    .send({ from: accounts[0], gas: "1000000" });
+  console.log("Contract deployed to", result.options.address);
+  provider.engine.stop();
+};
+
+deploy();
