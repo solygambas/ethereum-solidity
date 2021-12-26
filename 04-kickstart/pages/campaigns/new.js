f1c78c829c0cdb1847302d0a70d2dc2c4afa8c16
solygambas <51904909+solygambas@users.noreply.github.com>
2021-12-26 09:38:56 +0100

    add submit

--- 04-kickstart/pages/campaigns/new.js
+++ 04-kickstart/pages/campaigns/new.js
@@ -1,14 +1,25 @@
 import { useState } from "react";
 import { Button, Form, Input } from "semantic-ui-react";
 
+import factory from "../../ethereum/factory";
+import web3 from "../../ethereum/web3";
 import Layout from "../../components/Layout";
 
 function CampaignNew() {
   const [minimumContribution, setMinimumContribution] = useState("");
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    const accounts = await web3.eth.getAccounts();
+    await factory.methods
+      .createCampaign(minimumContribution)
+      .send({ from: accounts[0] });
+  };
+
   return (
     <Layout title="Create a Campaign">
       <h3>Create a Campaign</h3>
-      <Form>
+      <Form onSubmit={onSubmit}>
         <Form.Field>
           <label>Minimum Contribution</label>
           <Input
