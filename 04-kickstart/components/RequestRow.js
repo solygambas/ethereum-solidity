9b6425bfce99549635d4f4013d687d2719b74f27
solygambas <51904909+solygambas@users.noreply.github.com>
2021-12-29 17:34:45 +0100

    add finalizeRequest

--- 04-kickstart/components/RequestRow.js
+++ 04-kickstart/components/RequestRow.js
@@ -5,7 +5,8 @@ import getCampaign from "../ethereum/campaign";
 
 function RequestRow({ request, address, id, approversCount }) {
   const { Row, Cell } = Table;
-  const { description, value, recipient, approvalCount } = request;
+  const { description, value, recipient, approvalCount, complete } = request;
+  const readyToFinalize = approvalCount > approversCount / 2;
 
   const onApprove = async () => {
     const campaign = getCampaign(address);
@@ -15,8 +16,14 @@ function RequestRow({ request, address, id, approversCount }) {
     });
   };
 
+  const onFinalize = async () => {
+    const campaign = getCampaign(address);
+    const accounts = await web3.eth.getAccounts();
+    await campaign.methods.finalizeRequest(id).send({ from: accounts[0] });
+  };
+
   return (
-    <Row>
+    <Row disabled={complete} positive={readyToFinalize && !complete}>
       <Cell>{id}</Cell>
       <Cell>{description}</Cell>
       <Cell>{web3.utils.fromWei(value, "ether")}</Cell>
@@ -25,9 +32,18 @@ function RequestRow({ request, address, id, approversCount }) {
         {approvalCount}/{approversCount}
       </Cell>
       <Cell>
-        <Button color="green" basic onClick={onApprove}>
-          Approve
-        </Button>
+        {complete ? null : (
+          <Button color="green" basic onClick={onApprove}>
+            Approve
+          </Button>
+        )}
+      </Cell>
+      <Cell>
+        {complete ? null : (
+          <Button color="teal" basic onClick={onFinalize}>
+            Finalize
+          </Button>
+        )}
       </Cell>
     </Row>
   );
