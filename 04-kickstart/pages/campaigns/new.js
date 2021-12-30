22e1cd9f514db7c3ebca90255f8be3483e144403
solygambas <51904909+solygambas@users.noreply.github.com>
2021-12-30 13:07:38 +0100

    add metaMask detection

--- 04-kickstart/pages/campaigns/new.js
+++ 04-kickstart/pages/campaigns/new.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 import { Button, Form, Input, Message } from "semantic-ui-react";
 
@@ -11,6 +11,22 @@ function CampaignNew() {
   const [minimumContribution, setMinimumContribution] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
   const [loading, setLoading] = useState(false);
+  const [checkingMetaMask, setCheckingMetaMask] = useState(false);
+  const [isRinkebyNetwork, setIsRinkebyNetwork] = useState(false);
+  const [connected, setConnected] = useState(false);
+  const [connecting, setConnecting] = useState(false);
+
+  const getAccount = async () => {
+    setConnecting(true);
+    try {
+      await window.ethereum.request({ method: "eth_requestAccounts" });
+      router.reload();
+    } catch (error) {
+      setConnecting(false);
+    }
+  };
+
+  const handleAccountsChanged = (_accounts) => router.reload();
 
   const onSubmit = async (e) => {
     e.preventDefault();
@@ -28,24 +44,79 @@ function CampaignNew() {
     setLoading(false);
   };
 
+  useEffect(() => {
+    let cancelled = false;
+    if (
+      typeof window !== "undefined" &&
+      typeof window.ethereum !== "undefined"
+    ) {
+      setCheckingMetaMask(true);
+      const { ethereum } = window;
+      const checkRinkeby = async () => {
+        if (!cancelled) {
+          const chainId = await ethereum.request({
+            method: "eth_chainId",
+          });
+          if (chainId === "0x4") {
+            setIsRinkebyNetwork(true);
+            const accounts = await ethereum.request({
+              method: "eth_accounts",
+            });
+            if (accounts.length > 0 && ethereum.isConnected()) {
+              setConnected(true);
+            }
+          }
+          ethereum.on("accountsChanged", handleAccountsChanged);
+        }
+      };
+      checkRinkeby();
+    } else {
+      setCheckingMetaMask(false);
+    }
+
+    return () => {
+      cancelled = true;
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <Layout title="Create a Campaign">
       <h3>Create a Campaign</h3>
-      <Form onSubmit={onSubmit} error={!!errorMessage}>
-        <Form.Field>
-          <label>Minimum Contribution</label>
-          <Input
-            label="wei"
-            labelPosition="right"
-            value={minimumContribution}
-            onChange={(e) => setMinimumContribution(e.target.value)}
-          />
-        </Form.Field>
-        <Message error header="Oops!" content={errorMessage} />
-        <Button type="submit" primary loading={loading}>
-          Create
+      {!checkingMetaMask && (
+        <p>
+          MetaMask is required to run this app! Please install MetaMask and then
+          refresh this page.
+        </p>
+      )}
+      {checkingMetaMask && !isRinkebyNetwork && (
+        <p>
+          You must be connected to the <strong>Rinkeby test network</strong> for
+          Ether transactions made via this app.
+        </p>
+      )}
+      {checkingMetaMask && isRinkebyNetwork && !connected && (
+        <Button onClick={getAccount} disabled={connecting}>
+          Connect with MetaMask
         </Button>
-      </Form>
+      )}
+      {checkingMetaMask && isRinkebyNetwork && connected && (
+        <Form onSubmit={onSubmit} error={!!errorMessage}>
+          <Form.Field>
+            <label>Minimum Contribution</label>
+            <Input
+              label="wei"
+              labelPosition="right"
+              value={minimumContribution}
+              onChange={(e) => setMinimumContribution(e.target.value)}
+            />
+          </Form.Field>
+          <Message error header="Oops!" content={errorMessage} />
+          <Button type="submit" primary loading={loading}>
+            Create
+          </Button>
+        </Form>
+      )}
     </Layout>
   );
 }
