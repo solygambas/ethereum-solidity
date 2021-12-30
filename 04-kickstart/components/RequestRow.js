8a740a78eddfbbfd62472c3f2a05f74f8c004f05
solygambas <51904909+solygambas@users.noreply.github.com>
2021-12-30 13:49:17 +0100

    add metaMask detection on request list

--- 04-kickstart/components/RequestRow.js
+++ 04-kickstart/components/RequestRow.js
@@ -1,4 +1,6 @@
 import { Table, Button } from "semantic-ui-react";
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
 
 import web3 from "../ethereum/web3";
 import getCampaign from "../ethereum/campaign";
@@ -7,6 +9,22 @@ function RequestRow({ request, address, id, approversCount }) {
   const { Row, Cell } = Table;
   const { description, value, recipient, approvalCount, complete } = request;
   const readyToFinalize = approvalCount > approversCount / 2;
+  const [checkingMetaMask, setCheckingMetaMask] = useState(false);
+  const [isRinkebyNetwork, setIsRinkebyNetwork] = useState(false);
+  const [connected, setConnected] = useState(false);
+  const [connecting, setConnecting] = useState(false);
+
+  const getAccount = async () => {
+    setConnecting(true);
+    try {
+      await window.ethereum.request({ method: "eth_requestAccounts" });
+      router.reload();
+    } catch (error) {
+      setConnecting(false);
+    }
+  };
+
+  const handleAccountsChanged = (_accounts) => router.reload();
 
   const onApprove = async () => {
     const campaign = getCampaign(address);
@@ -22,6 +40,42 @@ function RequestRow({ request, address, id, approversCount }) {
     await campaign.methods.finalizeRequest(id).send({ from: accounts[0] });
   };
 
+  useEffect(() => {
+    let cancelled = false;
+    if (
+      typeof window !== "undefined" &&
+      typeof window.ethereum !== "undefined"
+    ) {
+      setCheckingMetaMask(true);
+      const { ethereum } = window;
+      const checkRinkeby = async () => {
+        if (!cancelled) {
+          const chainId = await ethereum.request({
+            method: "eth_chainId",
+          });
+          if (chainId === "0x4") {
+            setIsRinkebyNetwork(true);
+            const accounts = await ethereum.request({
+              method: "eth_accounts",
+            });
+            if (accounts.length > 0 && ethereum.isConnected()) {
+              setConnected(true);
+            }
+          }
+          ethereum.on("accountsChanged", handleAccountsChanged);
+        }
+      };
+      checkRinkeby();
+    } else {
+      setCheckingMetaMask(false);
+    }
+
+    return () => {
+      cancelled = true;
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <Row disabled={complete} positive={readyToFinalize && !complete}>
       <Cell>{id}</Cell>
@@ -32,18 +86,20 @@ function RequestRow({ request, address, id, approversCount }) {
         {approvalCount}/{approversCount}
       </Cell>
       <Cell>
-        {complete ? null : (
+        {checkingMetaMask && isRinkebyNetwork && connected && !complete && (
           <Button color="green" basic onClick={onApprove}>
             Approve
           </Button>
         )}
+        {checkingMetaMask && isRinkebyNetwork && connected && complete && null}
       </Cell>
       <Cell>
-        {complete ? null : (
+        {checkingMetaMask && isRinkebyNetwork && connected && !complete && (
           <Button color="teal" basic onClick={onFinalize}>
             Finalize
           </Button>
         )}
+        {checkingMetaMask && isRinkebyNetwork && connected && complete && null}
       </Cell>
     </Row>
   );
